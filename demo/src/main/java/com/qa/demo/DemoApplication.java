20104c3bf9daf98f5427e2cde14d90f26835973d
AGQA2024 <codesoftware2579@gmail.com>
2024-03-18 15:00:22 +0000

    Adding files.

--- demo/src/main/java/com/qa/demo/DemoApplication.java
+++ demo/src/main/java/com/qa/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +14,16 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
+	}
+
+//	@GetMapping("/hello")
+//	public String greeting(){
+//		return "Hello, World!";
+//	}
+
+	@GetMapping("/hello")
+	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name){
+		return String.format("Hello %s!", name);
 	}
 
 //	@GetMapping("/action")	//This makes the following accessible to our webserver?
