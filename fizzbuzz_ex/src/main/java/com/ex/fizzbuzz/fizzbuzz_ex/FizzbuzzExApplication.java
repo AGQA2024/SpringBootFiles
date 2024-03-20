e3d1ac7e05e5d6b8f6d219d6cae01bb579496ccc
AGQA2024 <codesoftware2579@gmail.com>
2024-03-20 14:49:37 +0000

    Adding mini-project files.

---
+++ fizzbuzz_ex/src/main/java/com/ex/fizzbuzz/fizzbuzz_ex/FizzbuzzExApplication.java
@@ -0,0 +1,13 @@
+package com.ex.fizzbuzz.fizzbuzz_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzbuzzExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzbuzzExApplication.class, args);
+	}
+
+}
---
+++ fizzbuzz_ex/src/test/java/com/ex/fizzbuzz/fizzbuzz_ex/FizzbuzzExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.fizzbuzz.fizzbuzz_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzbuzzExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
